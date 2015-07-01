186a938df3f0a8d1c820fe4e737d3ca4df1a4190
Bob Weigel <rweigel@gmu.edu>
2015-07-01 18:37:28 -0400

    misc

--- gic/prepareData.m
+++ gic/prepareData.m
@@ -1,83 +1,139 @@
-function [B_JMA,E_JMA] = prepareData(short)
+function [Bi,dBi,Ei] = prepareData(short,long)
 
 % 1-second B and E data from http://www.kakioka-jma.go.jp/metadata?locale=en
 
-if strmatch(short,'kak')
-    long = 'kakioka';
-end
-if strmatch(short,'mem')
-    long = 'memambetsu';
-end
+bname = sprintf('./data/%s/Bfile.mat',long);
+ename = sprintf('./data/%s/Efile.mat',long);
+bname2 = sprintf('./data/%s/Bfile2.mat',long);
+ename2 = sprintf('./data/%s/Efile2.mat',long);
 
-bname = sprintf('./data/%s/JMA_Bfile.mat',long);
+if ~exist(bname2)
 if ~exist(bname)
-    JMA_Bfile = [];
+    Bfile = [];
     for i = 1:31
         fname = sprintf('data/%s/unzip/%s200612%02ddsec.sec',long,short,i);
         fprintf('Reading %s\n',fname);
         com = sprintf('grep "^[0-9]" %s | sed "s/-/ /g" | sed "s/:/ /g" > tmp.txt',fname);
         system(com);
         load tmp.txt
-        JMA_Bfile = [JMA_Bfile;tmp];
+        Bfile = [Bfile;tmp];
     end
-    save(bname,'JMA_Bfile')
+    save(bname,'Bfile')
 else
     fprintf('Reading %s\n',bname);
     load(bname)
 end
+end
 
-clear Atmp;
-bname = sprintf('./data/%s/JMA_Efile.mat',long);
-if ~exist(bname)
-    JMA_Efile = [];
+if ~exist(ename2)
+if ~exist(ename)
+    Efile = [];
     for i = 1:31
         fname = sprintf('data/%s/unzip/%s200612%02ddgef.sec',long,short,i);
         fprintf('Reading %s\n',fname);
         com = sprintf('grep "^[0-9]" %s | sed "s/-/ /g" | sed "s/:/ /g" > tmp.txt',fname);
         system(com);
         load tmp.txt
-        JMA_Efile= [JMA_Efile;tmp];
+        Efile= [Efile;tmp];
     end
-    save(bname,'JMA_Efile')
+    save(ename,'Efile')
 else
-    fprintf('Reading %s\n',bname);
-    load(bname)
+    fprintf('Reading %s\n',ename);
+    load(ename)
+end
 end
 
-fprintf('Subtracting off means of non NaN values for B.\n');
+if ~exist(bname2)
+    fprintf('Subtracting off means of non NaN values for B.\n');
 
-t_JMA_B = datenum(JMA_Bfile(1:6));
-B_JMA   = JMA_Bfile(:,8:10);
+    t_B = datenum(Bfile(1:6));
+    B   = Bfile(:,8:10);
 
-for i = 1:3
-    Ig = find(B_JMA(:,i) ~= 88888.00);
-    B_JMA(:,i) = B_JMA(:,i)-mean(B_JMA(Ig,i));
-    Ib = find(B_JMA(:,i) == 88888.00);
-    B_JMA(Ib,i) = NaN;
+    if strmatch(short,'mmb')
+        fill = 88888.00;
+    end
+    if strmatch(short,'kak')
+        fill = 99999.00;
+    end
+
+    for i = 1:3
+        Ig = find(B(:,i) ~= fill);
+        Ib = find(B(:,i) == fill);
+        B(Ig,i) = B(Ig,i)-mean(B(Ig,i));
+        B(Ib,i) = NaN;
+    end
+    
+    fprintf('Interpolating over NaNs values in B.\n');
+    ti = [1:size(B,1)]';
+    for i = 1:3
+        Ig = find(~isnan(B(:,i))); % Good points.
+        N  = size(B,1);
+        Nb = N-length(Ig);
+        Bg = B(Ig,i);
+        tg = ti(Ig);
+        Bi(:,i) = interp1(tg,Bg,ti);
+        fprintf('Interpolated over %d of %d points in component %d\n',Nb,N,i)
+    end
+
+    dB = diff(B);
+    dB(end+1,:) = dB(end,:); % Repeat last value so length is same as B
+    fprintf('Interpolating over NaNs values in dB/dt.\n');
+    ti = [1:size(dB,1)]';
+    for i = 1:3
+        Ig = find(~isnan(dB(:,i))); % Good points.
+        N  = size(dB,1);
+        Nb = N-length(Ig);
+        dBg = dB(Ig,i);
+        tg = ti(Ig);
+        dBi(:,i) = interp1(tg,dBg,ti);
+        fprintf('Interpolated over %d of %d points in component %d\n',Nb,N,i)
+end
+    save(bname2,'B','Bi','dB','dBi')
+else
+    fprintf('Reading %s\n',bname2);
+    load(bname2)
 end
 
-fprintf('Subtracting off means of non NaN values for B.\n');
+if ~exist(ename2)
 
-t_JMA_E = datenum(JMA_Efile(:,1:6));
-E_JMA = JMA_Efile(:,8:9);
+    t_E = datenum(Efile(:,1:6));
+    E = Efile(:,8:9);
 
-if strmatch(short,'kak')
     fprintf('Placing NaNs in bad areas of E.\n');
-    E_JMA(E_JMA > 8e4) = NaN;
+    if strmatch(short,'kak')
+        E(E > 8e4) = NaN;
+    end
+    if strmatch(short,'mmb')
+        Ixb = [1755000:1755600];
+        E(Ixb,1) = NaN;
+
+        Iyb = [1754600:1755100];
+        E(Iyb,2) = NaN;
+    end
+
+    fprintf('Subtracting off means of non NaN values for E.\n');
+
+    for i = 1:2
+        Ig = find(E(:,i) ~= 88888.00 & ~isnan(E(:,i)));
+        E(:,i) = E(:,i)-mean(E(Ig,i));
+        Ib = find(E(:,i) == 88888.00);
+        E(Ib,i) = NaN;
+    end
+
+    fprintf('Interpolating over NaNs values in E.\n');
+    ti = [1:size(E,1)]';
+    for i = 1:2
+        Ig = find(~isnan(E(:,i))); % Good points.
+        N  = size(E,1);
+        Nb = N-length(Ig);
+        Eg = E(Ig,i);
+        tg = ti(Ig);
+        Ei(:,i) = interp1(tg,Eg,ti);
+        fprintf('Interpolated over %d of %d points in component %d\n',Nb,N,i)
+    end
+    save(ename2,'E','Ei')
+else
+    fprintf('Reading %s\n',ename2);
+    load(ename2)
 end
 
-if strmatch(short,'mem')
-    fprintf('Placing NaNs in bad areas of E.\n');
-    Ixb = [1755000:1755600];
-    E_JMA(Ixb,1) = NaN;
-
-    Iyb = [1754600:1755100];
-    E_JMA(Iyb,2) = NaN;
-end
-
-for i = 1:2
-    Ig = find(E_JMA(:,i) ~= 88888.00 & ~isnan(E_JMA(:,i)));
-    E_JMA(:,i) = E_JMA(:,i)-mean(E_JMA(Ig,i));
-    Ib = find(E_JMA(:,i) == 88888.00);
-    E_JMA(Ib,i) = NaN;
-end
