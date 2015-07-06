92107d29feeb5dd4b1a3a84836d2760188d77c19
Bob Weigel <rweigel@gmu.edu>
2015-07-06 10:38:26 -0400

    Merge clean-up

--- gic/zplanewave_demo1.m
+++ gic/zplanewave_demo1.m
@@ -16,47 +16,27 @@ f = [1:N/2]/N;
 if (profile == 1)
     h = [];
     s = 1;
-<<<<<<< HEAD
-    titlestr = sprintf('$\\mbox{Infinite half space;}\\quad\\rho = %.1f \\mbox{ [Ohm}\\cdot\\mbox{m]}',1/s);
-=======
     titlestr = sprintf('$\\mbox{Infinite half space}\\quad\\rho = %.1f \\mbox{ [Ohm}\\cdot\\mbox{m]}',1/s);
->>>>>>> d445017373976f894afa1634861e2967775694ca
 end
 if (profile == 2)
     s = [1/1000,1/10,1/100,1/5];
     h = 1e3*[10,20,400];
-<<<<<<< HEAD
-    titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]};\\quad h = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
-=======
     titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]}\\quad t = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
->>>>>>> d445017373976f894afa1634861e2967775694ca
 end
 if (profile == 3)
     s = [1/10,1/100,1/1000];
     h = 1e3*[1,10];
-<<<<<<< HEAD
-    titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]};\\quad h = [%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
-=======
     titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]}\\quad t = [%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
->>>>>>> d445017373976f894afa1634861e2967775694ca
 end
 if (profile == 4)
     s = [10,0.1,10,0.1];
     h = 1e3*[10,10,10];
-<<<<<<< HEAD
-    titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]};\\quad h = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
-=======
     titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]}\\quad t = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
->>>>>>> d445017373976f894afa1634861e2967775694ca
 end
 if (profile == 5)
     s = [0.1,10,0.1,10];
     h = 1e3*[10,10,10];
-<<<<<<< HEAD
-    titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]};\\quad h = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
-=======
     titlestr = sprintf('$\\rho = [%.1f,%.1f,%.1f,%.1f] \\mbox{ [Ohm}\\cdot\\mbox{m]}\\quad t = [%.1f,%.1f,%.1f,\\infty] \\mbox{[km]}',[1./s,h/1e3]);
->>>>>>> d445017373976f894afa1634861e2967775694ca
 end
 
 titlestr = [titlestr,sprintf('\\quad \\Delta f =1/(%d\\cdot 10^%d)$',no,ne)];
@@ -95,11 +75,7 @@ figure(1);clf;
                 ' $\widetilde{Z}=\|\widetilde{E}_x/\widetilde{B}_y\| = \omega\|\widetilde{C}\|\quad\mbox{[m/s]}$'...
             );
     set(lh,'Interpreter','Latex');
-<<<<<<< HEAD
-    print('-dpng','-r600',sprintf('figures/%s_transferfn_profile_%d.png',base,profile));
-=======
     print('-dpng','-r150',sprintf('figures/%s_transferfn_profile_%d.png',base,profile));
->>>>>>> d445017373976f894afa1634861e2967775694ca
     print('-depsc',sprintf('figures/%s_transferfn_profile_%d.eps',base,profile));
     fprintf('Wrote figures/%s_transferfn_profile_%d.{png,eps}\n',base,profile);
     
@@ -118,40 +94,6 @@ figure(2);clf;
                 ' $\phi_{\widetilde{Z}}\quad\mbox{[deg]}$',...
                 'Location','NorthWest');
     set(lh,'Interpreter','Latex');
-<<<<<<< HEAD
-    print('-dpng','-r600',sprintf('figures/%s_phase_profile_%d.png',base,profile));
-    print('-dpng',sprintf('figures/%s_phase_profile_%d.eps',base,profile));
-    fprintf('Wrote figures/%s_phase_profile_%d.{png,eps}\n',base,profile)
-
-fignum = 3;
-if (~isempty(h))
-    figure(fignum);clf;
-        fignum = fignum + 1;
-        d = cumsum(h);
-        d = [10^(round(log10(d(1)))-1),d,10^(round(log10(d(end)))+1)];
-        s(end+1) = s(end);
-        for i = 1:length(s)-1
-            loglog([1/s(i),1/s(i)],[d(i),d(i+1)]/1e3,'LineWidth',5);
-            hold on;
-            loglog([1/s(i),1/s(i+1)],[d(i+1),d(i+1)]/1e3,'LineWidth',1);
-        end
-        grid on;
-        % Add some space to left and right.
-        xl = get(gca,'XLim');
-        set(gca,'XLim',[xl(1)*0.7,xl(2)*1.1]);
-        th = title(titlestr);
-        set(th,'Interpreter','Latex');  
-
-        set(gca,'YDir','reverse');
-        %set(gca,'XAxisLocation','Top');
-        xlabel('Resistivity [\Omega\cdotm]');
-        ylabel('Depth [km]');
-        print('-dpng','-r600',sprintf('figures/%s_geometry_profile_%d.png',base,profile));
-        print('-depsc',sprintf('figures/%s_geometry_profile_%d.eps',base,profile));
-        fprintf('Wrote figures/%s_geometry_profile_%d.{png,eps}\n',base,profile)
-end
-=======
-    print('-dpng','-r150',sprintf('figures/%s_phase_profile_%d.png',base,profile));
     print('-dpng',sprintf('figures/%s_phase_profile_%d.eps',base,profile));
     fprintf('Wrote figures/%s_phase_profile_%d.{png,eps}\n',base,profile)
 
@@ -185,17 +127,11 @@ figure(3);clf;
     print('-depsc',sprintf('figures/%s_geometry_profile_%d.eps',base,profile));
     fprintf('Wrote figures/%s_geometry_profile_%d.{png,eps}\n',base,profile)
 
->>>>>>> d445017373976f894afa1634861e2967775694ca
-
 tft  = [-N/2:1:N/2];
 dhft = fftshift(ifft(dZf));
 hft  = fftshift(ifft(Zf));
 
-<<<<<<< HEAD
-figure(fignum);clf;
-=======
 figure(4);clf;
->>>>>>> d445017373976f894afa1634861e2967775694ca
     plot(tft,hft,'k','LineWidth',2);
     hold on;grid on;
     plot(tft,dhft,'g','LineWidth',2);
@@ -207,11 +143,7 @@ figure(4);clf;
     lh = legend('$E_x(t)\mbox{ for }B_y(t) = \delta(t)$',...
                 '$E_x(t)\mbox{ for }B''_y(t) = \delta(t)$');
     set(lh,'Interpreter','Latex');
-<<<<<<< HEAD
-    print('-dpng','-r600',sprintf('figures/%s_IRF_profile_%d.png',base,profile));
-=======
     print('-dpng','-r150',sprintf('figures/%s_IRF_profile_%d.png',base,profile));
->>>>>>> d445017373976f894afa1634861e2967775694ca
     print('-depsc',sprintf('figures/%s_IRF_profile_%d.eps',base,profile));
     fprintf('Wrote figures/%s_IRF_profile_%d.{png,eps}\n',base,profile)
     
