acdfc891f40022b6c5c92fcaa1b5b07b7f93c1a3
Bob Weigel <rweigel@gmu.edu>
2015-09-01 18:55:08 -0400

    Fixes

--- gic/transferfn/Hpredict.m
+++ gic/transferfn/Hpredict.m
@@ -2,6 +2,6 @@ function Ep = Hpredict(t,H,B)
 
 N        = find(t == 0);
 tmp      = filter(H(:,1),1,B(:,2));
-Ep(:,1)  = tmp(N+1:end);
+Ep(:,1)  = tmp(N:end);
 tmp      = filter(H(:,2),1,B(:,1));
-Ep(:,2)  = tmp(N+1:end);
+Ep(:,2)  = tmp(N:end);
