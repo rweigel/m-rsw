6e18ebe5d16595194c26834bb847e429308692cf
Bob Weigel <rweigel@gmu.edu>
2016-02-22 14:07:25 -0500

    IRIS improvements

--- gic/getIRIS.m
+++ gic/getIRIS.m
@@ -1,17 +1,27 @@
-
 % http://service.iris.edu/irisws/fedcatalog/1/query?net=EM
 % Get actual last day in archive from bottom of
 % http://www.iris.washington.edu/mda/EM/MBB05
 
 sta = 'MBB05';
+start = '2008-06-02';
+stop  = '2011-05-27';
+chas  = {'LFE','LFN','LFZ','LQE','LQN'};
 
+sta = 'MBB05';
 start = '2009-05-11';
 stop  = '2011-05-27';
 chas  = {'MFE','MFN','MFZ','MQE','MQN'};
 
-start = '2008-06-02';
-stop  = '2011-05-27';
-chas  = {'LFE','LFN','LFZ','LQE','LQN'};
+sta = 'WYH18';
+C = getIRISCatalog();
+for i = 1:length(C)
+    if strmatch(sta,C{i})
+        start = C{i}{2}(1:10);
+        stop = C{i}{3}(1:10);
+        chas = C{i}(4:end);
+        break
+    end
+end
 
 url0 = 'http://service.iris.edu/irisws/timeseries/1/query?net=EM&';
 url1 = 'sta=%s&loc=--&cha=%s&correct=true&starttime=%sT00:00:00&endtime=%sT23:59:59&output=ascii';
@@ -19,8 +29,8 @@ url1 = 'sta=%s&loc=--&cha=%s&correct=true&starttime=%sT00:00:00&endtime=%sT23:59
 startdn = datenum(start);
 stopdn  = datenum(stop);
 
-if ~exist('data')
-    mkdir('data')
+if ~exist(['data/iris/',sta])
+    mkdir(['data/iris/',sta])
 end
 
 for i = startdn:stopdn
@@ -32,22 +42,28 @@ for i = startdn:stopdn
             fprintf('Found %s. Not re-downloading.\n',fname);
             continue
         end
-        fprintf('Downloading %s/%s on %s ... ',sta,chas{j},ds);
+        fprintf('Downloading %s/%s on %s ...\n',sta,chas{j},ds);
         try
-            urlwrite(url,fname);
+            [f,s] = urlwrite(url,fname);
             fprintf('Done. ');
+
             fname2 = [fname(1:end-4),'.dat'];
             fname3 = [fname(1:end-4),'.mat'];
-            com = sprintf('perl -p -e ''s/([0-9])\\-([0-9])/$1 $2/g'' %s | perl -p -e ''s/T|:/ /g'' | perl -p -e ''/^[0-9].*/g'' | grep -e "^[0-9]" > %s',fname,fname2);
-            % Faster than using MATLAB fscanf to read.
-            % Crate temp file with no non-numeric chars then use MATLAB load.
-            system(com);
-            X = load(fname2);
-            delete(fname2);
-            save(fname3,'X');
-            fprintf('Saved %s\n',fname3);
+            if ~exist(fname)
+                fprintf('No data for %s.\n',fname);
+            else
+                com = sprintf('perl -p -e ''s/([0-9])\\-([0-9])/$1 $2/g'' %s | perl -p -e ''s/T|:/ /g'' | perl -p -e ''/^[0-9].*/g'' | grep -e "^[0-9]" > %s',fname,fname2);
+                % Faster than using MATLAB fscanf to read.
+                % Crate temp file with no non-numeric chars then use MATLAB load.
+                system(com);
+                X = load(fname2);
+                delete(fname2);
+                save(fname3,'X');
+                fprintf('  Saved %s\n',fname3);
+            end
         catch
-            fprintf('Request failed for %s\n',url);
+            fprintf('  Request failed for %s\n',url);
+            s
         end
     end
 end
