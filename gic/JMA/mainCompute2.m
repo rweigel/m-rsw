02639e02e4cd444118232ca7f4d5f32857be171b
Bob Weigel <rweigel@gmu.edu>
2018-02-13 15:35:09 -0500

    fix for TF size

--- gic/JMA/mainCompute2.m
+++ gic/JMA/mainCompute2.m
@@ -6,7 +6,7 @@ Na = 60*10; % Number of acausal coefficients
 %Na = 10; % Number of acausal coefficients
 
 % Compute transfer function with B driving E using FD method
-[Z_FD,fe_FD,H_FD,t_FD,Ep_FD] = transferfnFD(B(Ix,:),E(Ix,:),2,'rectangular');
+[Z_FD,fe_FD,H_FD,t_FD,Ep_FD] = transferfnFD(B(Ix,1:2),E(Ix,:),2,'rectangular');
 
 % Compute transfer function with E driving GIC using FD method
 % Note - should be using GIC(Ix,2), but transferfnFD does not handle that
@@ -14,10 +14,10 @@ Na = 60*10; % Number of acausal coefficients
 [Z2_FD,fe2_FD,H2_FD,t2_FD,GICp2_FD] = transferfnFD(E(Ix,:),GIC(Ix,:),1,'rectangular');
 
 % Compute transfer function with B driving GIC using FD method
-[Z3_FD,fe3_FD,H3_FD,t3_FD,GICp3_FD] = transferfnFD(B(Ix,:),GIC(Ix,:),1,'rectangular');
+[Z3_FD,fe3_FD,H3_FD,t3_FD,GICp3_FD] = transferfnFD(B(Ix,1:2),GIC(Ix,:),1,'rectangular');
 
 % Compute transfer function with B driving E using TD method
-[Z_TD,f_TD,H_TD,t_TD,Ep_TD] = transferfnTD(B(Ix,:),E(Ix,:),Nc,Na);
+[Z_TD,f_TD,H_TD,t_TD,Ep_TD] = transferfnTD(B(Ix,1:2),E(Ix,:),Nc,Na);
 
 % Compute transfer function with E driving GIC using TD method
 [Z2_TD,f2_TD,H2_TD,t2_TD,GICp2_TD] = transferfnTD(E(Ix,:),GIC(Ix,2),Nc,Na);
@@ -33,7 +33,7 @@ GICp2_TD(:,1) = NaN*GICp2_TD(:,1);
 [Z2b_TD,f2b_TD,H2b_TD,t2b_TD,GICp2b_TD] = transferfnTD(E(Ix,2),GIC(Ix,2),Nc,Na);
 
 % Compute transfer function with B driving GIC using TD method
-[Z3_TD,f3_TD,H3_TD,t3_TD,GICp3_TD] = transferfnTD(B(Ix,:),GIC(Ix,2),Nc,Na);
+[Z3_TD,f3_TD,H3_TD,t3_TD,GICp3_TD] = transferfnTD(B(Ix,1:2),GIC(Ix,2),Nc,Na);
 GICp3_TD(:,2) = GICp3_TD(:,1);
 GICp3_TD(:,1) = NaN*GICp3_TD(:,1);
 
